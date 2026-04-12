a4a1668229e6fdd41ce1d8d25b128a4cddb7fd30
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-04-13 01:21:18 +0500

    Fix a bug in darray expand capacity logic, all tests pass

--- src/darray.c
+++ src/darray.c
@@ -5,15 +5,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define COPY_DATA_AT_INDEX(darr, data, index)\
+#define COPY_DATA_AT_INDEX(arr, index, data, data_size)\
     do {\
         const unsigned char* data_src = (const unsigned char*) data;\
-        unsigned char* data_dest = darr->arr + (darr->data_size * index);\
-        for (size_t i = 0; i < darr->data_size; i++)\
-        {\
-            data_dest[i] = data_src[i];\
+        unsigned char* data_dest = arr + (data_size * index);\
+        for (size_t data_byte_i = 0; data_byte_i < data_size; data_byte_i++) {\
+            data_dest[data_byte_i] = data_src[data_byte_i];\
         }\
     } while(0)
+
+static void darray_expand(DArray* darr)
+{
+    size_t new_capacity = 2 * darr->capacity;
+    assert_always(new_capacity > darr->capacity, "Overflow happened! The array has gotten too big!");
+
+    unsigned char* new_arr = NULL;
+    new_arr = (unsigned char*) malloc(darr->data_size * new_capacity);
+    assert_always(new_arr != NULL, "Failed to allocate memory for a new capacity.");
+
+    for (size_t i = 0; i < darr->length; i++)
+    {
+        COPY_DATA_AT_INDEX(
+            new_arr,
+            i,
+            darray_get_at(darr, i),
+            darr->data_size
+        );
+    }
+
+    darr->capacity = new_capacity;
+    free(darr->arr);
+    darr->arr = new_arr;
+}
 
 DArray* init_darray(DArray* darr, const size_t capacity, const size_t data_size)
 {
@@ -60,7 +83,7 @@ void darray_set_at(DArray* darr, const size_t index, const void* data, const siz
     assert_always(index < darr->length, "Index is out of bounds");
     assert_always(darr->data_size == data_size, "Incompatible data size");
     
-    COPY_DATA_AT_INDEX(darr, data, index);
+    COPY_DATA_AT_INDEX(darr->arr, index, data, data_size);
 }
 
 void darray_add(DArray* darr, const void* data, const size_t data_size)
@@ -68,22 +91,11 @@ void darray_add(DArray* darr, const void* data, const size_t data_size)
     assert_always(darr != NULL, "NULL object of type DArray was passed");
     assert_always(darr->data_size == data_size, "Incompatible data size");
 
-    if (darr->length == darr->capacity) {
-        size_t new_capacity = 2 * darr->capacity;
-        unsigned char* new_arr = NULL;
-        new_arr = malloc(darr->data_size * new_capacity);
-        assert_always(new_arr != NULL, "Failed to allocate memory for a new capacity.");
+    if (darr->length == darr->capacity)
+        darray_expand(darr);
 
-        for (size_t i = 0; i < darr->capacity; i++)
-        {
-            new_arr[i] = darr->arr[i];
-        }
-
-        darr->capacity = new_capacity;
-        free(darr->arr);
-        darr->arr = new_arr;
-    }
-
+    COPY_DATA_AT_INDEX(darr->arr, darr->length, data, data_size);
     darr->length++;
-    COPY_DATA_AT_INDEX(darr, data, darr->length);
 }
+
+
--- test/darray_test.c
+++ test/darray_test.c
@@ -8,11 +8,11 @@
     do {\
         const unsigned char* retrieved_data = (const unsigned char*) darray_get_at(darr, index);\
         for (size_t data_byte_i = 0; data_byte_i < data_size; data_byte_i++) {\
-            ASSERT_TRUE(expected_data[data_byte_i] == retrieved_data[data_byte_i]);\
+            ASSERT_TRUE(retrieved_data[data_byte_i] == expected_data[data_byte_i]);\
         }\
     } while(0)
 
-TEST(testInitDArray, "Tests a DArray object initialization")
+TEST(testInitDArray, "GIVEN NULL DArray pointer, WHEN DArray init, THEN create a new DArray object and initialize it with proper data.")
 {
     // GIVEN
     const size_t expected_capacity = 16;
@@ -27,7 +27,7 @@ TEST(testInitDArray, "Tests a DArray object initialization")
     ASSERT_TRUE(darr->arr != NULL);
 }
 
-TEST(testFreeInitializedDArray, "Tests an initialized DArray object free")
+TEST(testFreeInitializedDArray, "GIVEN initialized DArray object, WHEN free DArray, THEN properly free the resources.")
 {
     DArray* darr = init_darray(NULL, 16, 8);
     ASSERT_TRUE(darr != NULL);
@@ -41,32 +41,44 @@ TEST(testFreeNULLDArray, "Tests a NULL DArray object free")
     free_darray(&darr);
 }
 
-TEST(test_no_expand_when_DArray_add, "Test DArray add doesn't expand the array capacity when a new element added.")
+TEST(testDArrayAdd, "GIVEN an initialized DArray object with 0 length, WHEN add a new element, THEN the copy of the data should exist in the array")
 {
     // GIVEN
     size_t data_size = 8;
-    size_t expected_capacity = 16;
-    DArray* darr = init_darray(NULL, expected_capacity, data_size);
+    size_t capacity = 16;
+    DArray* darr = init_darray(NULL, capacity, data_size);
     ASSERT_TRUE(darr != NULL);
-    ASSERT_TRUE(darr->capacity == expected_capacity); // Before add
     unsigned char data[data_size];
+    data[0] = 0x12;
+    data[1] = 0x34;
+    data[2] = 0xfd;
+    data[3] = 0xfe;
+    data[4] = 0xaf;
+    data[5] = 0xae;
+    data[6] = 0xef;
+    data[7] = 0xfe;
 
     // WHEN
     darray_add(darr, data, sizeof(data));
 
     // THEN
-    ASSERT_TRUE(darr->capacity == expected_capacity); // After add
+    ASSERT_TRUE(darr->length == 1);
+    ASSERT_TRUE(darr->capacity == capacity);
+
+    const unsigned char* data_retrieved = (const unsigned char*) darray_get_at(darr, 0);
+    for (size_t i = 0; i < data_size; i++) {
+        ASSERT_TRUE(data[i] == data_retrieved[i]);
+    }
 }
 
-TEST(test_expand_when_DArray_add, "GIVEN a DArray object with elements at full capacity, WHEN add a new element, THEN expand the capacity and copy the elements properly.")
+TEST(test_multiple_DArray_add, "GIVEN a DArray object with 0 elements, WHEN add multiple elements, THEN each added element is placed in the array in the added order.")
 {
     // GIVEN
     size_t data_size = 8;
-    size_t initial_capacity = 4;
-    size_t expected_capacity = 2 * initial_capacity;
-    DArray* darr = init_darray(NULL, initial_capacity, data_size);
+    size_t capacity = 16;
+    DArray* darr = init_darray(NULL, capacity, data_size);
     ASSERT_TRUE(darr != NULL);
-    ASSERT_TRUE(darr->capacity == initial_capacity);
+    ASSERT_TRUE(darr->capacity == capacity);
 
     unsigned char data[data_size];
     data[0] = 0x12;
@@ -77,6 +89,47 @@ TEST(test_expand_when_DArray_add, "GIVEN a DArray object with elements at full c
     data[5] = 0xae;
     data[6] = 0xef;
     data[7] = 0xfe;
+
+    // WHEN
+    ASSERT_TRUE(darr->length == 0);
+    darray_add(darr, data, sizeof(data));
+    ASSERT_TRUE(darr->length == 1);
+    darray_add(darr, data, sizeof(data));
+    ASSERT_TRUE(darr->length == 2);
+    darray_add(darr, data, sizeof(data));
+    ASSERT_TRUE(darr->length == 3);
+    darray_add(darr, data, sizeof(data));
+
+    // THEN
+    ASSERT_TRUE(darr->length == 4);
+    ASSERT_TRUE(darr->capacity == capacity);
+
+    for (size_t i = 0; i < darr->length; i++)
+    {
+        VERIFY_DATA_AT_INDEX(darr, i, data, data_size);
+    }
+}
+
+TEST(test_expand_when_DArray_add, "GIVEN a DArray object with elements at full capacity, WHEN add a new element, THEN expand the capacity and copy the elements properly.")
+{
+    // GIVEN
+    size_t data_size = 8;
+    size_t initial_capacity = 4;
+    size_t expected_capacity = 2 * initial_capacity;
+
+    DArray* darr = init_darray(NULL, initial_capacity, data_size);
+    ASSERT_TRUE(darr != NULL);
+    ASSERT_TRUE(darr->capacity == initial_capacity);
+
+    unsigned char data[data_size];
+    data[0] = 18;
+    data[1] = 52;
+    data[2] = 253;
+    data[3] = 254;
+    data[4] = 175;
+    data[5] = 174;
+    data[6] = 239;
+    data[7] = 45;
 
     darray_add(darr, data, sizeof(data));
     darray_add(darr, data, sizeof(data));
@@ -87,39 +140,11 @@ TEST(test_expand_when_DArray_add, "GIVEN a DArray object with elements at full c
     darray_add(darr, data, sizeof(data));
 
     // THEN
-    ASSERT_TRUE(darr->capacity == expected_capacity);
-    for (size_t i = 0; i < initial_capacity; i++)
+    ASSERT_TRUE(darr->length == 5);
+    ASSERT_TRUE(darr->capacity == expected_capacity); // the array is expanded
+
+    for (size_t i = 0; i < darr->length; i++)
     {
         VERIFY_DATA_AT_INDEX(darr, i, data, data_size);
-    }
-
-    VERIFY_DATA_AT_INDEX(darr, initial_capacity, data, data_size);
-}
-
-TEST(testDArrayAdd, "Tests DArray add")
-{
-    // GIVEN
-    size_t data_size = 8;
-    DArray* darr = init_darray(NULL, 16, data_size);
-    ASSERT_TRUE(darr != NULL);
-    unsigned char data[data_size];
-    data[0] = 0x12;
-    data[1] = 0x34;
-    data[2] = 0xfd;
-    data[3] = 0xfe;
-    data[4] = 0xaf;
-    data[5] = 0xae;
-    data[6] = 0xef;
-    data[7] = 0xfe;
-
-    // WHEN
-    darray_add(darr, data, sizeof(data));
-
-    // THEN
-    const unsigned char* data_retrieved = (const unsigned char*) darray_get_at(darr, 0);
-    for (size_t i = 0; i < data_size; i++) {
-        // ASSERT_TRUE(data[i] == data_retrieved[i]);
-        printf("data[%zu] = %hhu\tdata_retrieved[%zu] = %hhu\n",
-                i, data[i], i, data_retrieved[i]);
     }
 }
