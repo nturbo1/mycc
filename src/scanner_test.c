a6f3fd71ef4845cb769d0bf2fdb9529322ef3744
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-04-22 01:10:51 +0500

    Add init scanner test

--- src/scanner_test.c
+++ src/scanner_test.c
@@ -1,19 +1,21 @@
 #include "nctest.h"
 #include "scanner.h"
 
-// TEST(init_scanner,
-//      "Should allocate memory with correct values when a NULL scanner and an existing file are passed")
-// {
-//     Scanner* s = NULL;
-//     const char* filepath = "scanner_test.c";
-//     s = init_scanner(s, filepath);
-//
-//     ASSERT_TRUE(s != NULL);
-//     ASSERT_TRUE(s->file != NULL);
-//     ASSERT_TRUE(s->bf_end == SCANNER_BUFFER_SIZE);
-//     ASSERT_TRUE(s->next == 0);
-//     ASSERT_TRUE(s->ln_offset == 0);
-//     ASSERT_TRUE(s->col_offset == 0);
-//
-//     for ()
-// }
+TEST(init_scanner,
+     "GIVEN a NULL scanner passed to init_scanner with a valid filepath, WHEN init_scanner, "
+     "THEN initialize a scanner fields with correct values.")
+{
+    Scanner* s = NULL;
+    const char* filepath = "src/scanner_test.c";
+    s = init_scanner(s, filepath);
+
+    ASSERT_TRUE(s != NULL);
+    ASSERT_TRUE(s->file != NULL);
+    ASSERT_TRUE(s->bf_end == SCANNER_BUFFER_SIZE);
+    ASSERT_TRUE(s->next == 0);
+    ASSERT_TRUE(s->ln_offset == 0);
+    ASSERT_TRUE(s->col_offset == 0);
+    ASSERT_TRUE(s->keywords != NULL);
+
+    fclose(s->file);
+}
